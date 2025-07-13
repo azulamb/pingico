04e10cfa0a51c16eea6566739ceb93c1c563fd19
azulamb <1127785+azulamb@users.noreply.github.com>
2025-07-13 20:02:44 +0900

    Add command.

--- mod.ts
+++ mod.ts
@@ -1,1 +1,26 @@
-export * from './src/pingico.ts';
+import { pingico as convert } from './src/pingico.ts';
+
+export const pingico = convert;
+
+export async function pingFileToIco(
+  ...files: (string | URL)[]
+): Promise<Blob> {
+  return convert(
+    ...await Promise.all(files.map(async (file) => {
+      return await Deno.readFile(file);
+    })),
+  );
+}
+
+if (import.meta.main) {
+  if (Deno.args.length < 2) {
+    console.error('Usage: deno run --allow-read --allow-write mod.ts <file1> <file2> ... <output>');
+    Deno.exit(1);
+  }
+  const files = Deno.args.map((arg) =>{
+    return new URL(arg, import.meta.url);
+  });
+  const outputFile = <URL>files.pop();
+  const icoBlob = await pingFileToIco(...files);
+  await Deno.writeFile(outputFile, new Uint8Array(await icoBlob.arrayBuffer()));
+}
