72b5a1d5b33606d283017035ea444526b55e0103
azulamb <1127785+azulamb@users.noreply.github.com>
2025-07-13 18:24:12 +0900

    Add multiicon test.

--- tests/all.test.ts
+++ tests/all.test.ts
@@ -2,10 +2,25 @@ import * as assert from 'jsr:@std/assert';
 import { pingico } from '../src/pingico.ts';
 
 Deno.test('Single icon', async () => {
-  console.log('test');
   const png = await Deno.readFile('./tests/icon_16.png');
   const ico = await Deno.readFile('./tests/icon_16.ico');
   const blob = await pingico(png);
   const blobData = new Uint8Array(await blob.arrayBuffer());
   assert.assertEquals(blobData, ico, `Single icon data does not match.`);
+});
+
+Deno.test('Multi icon', async () => {
+  const pings = await Promise.all([
+    16,
+    24,
+    32,
+    48,
+    256,
+  ].map((size) => {
+    return Deno.readFile(`./tests/icon_${size}.png`);
+  }));
+  const ico = await Deno.readFile('./tests/icon.ico');
+  const blob = await pingico(...pings);
+  const blobData = new Uint8Array(await blob.arrayBuffer());
+  assert.assertEquals(blobData, ico, `Multi icon data does not match.`);
 });
